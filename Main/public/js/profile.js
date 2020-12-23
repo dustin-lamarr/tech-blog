b350cc445a2db8bc4307ee24de2e41e5fd766119
Dustin LaMarr <dustin.lamarr@gmail.com>
2020-12-23 11:28:41 -0700

    login/logout works. Create profile works. blogs load to main page, and route to blogs works

--- Main/public/js/profile.js
+++ Main/public/js/profile.js
@@ -2,13 +2,13 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#blog-name').value.trim();
-  const needed_funding = document.querySelector('#blog-funding').value.trim();
   const description = document.querySelector('#blog-desc').value.trim();
+  const content = document.querySelector('#blog-content').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && description && content) {
     const response = await fetch(`/api/blogs`, {
       method: 'POST',
-      body: JSON.stringify({ name, description }),
+      body: JSON.stringify({ name, description, content }),
       headers: {
         'Content-Type': 'application/json',
       },
